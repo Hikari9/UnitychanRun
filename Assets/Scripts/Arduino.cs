684a306c03044e4bc1b1569db86ae471b63abd53
Rico Tiongson <thericotiongson@gmail.com>
2015-02-24 17:12:46 +0800

    Appropriated Unity scripts for Arduino.

--- Assets/Scripts/Arduino.cs
+++ Assets/Scripts/Arduino.cs
@@ -1,16 +1,27 @@
 ﻿using UnityEngine;
 using System;
 using System.Collections;
+using System.IO;
 using System.IO.Ports;
 using System.Threading;
 
 public class Arduino : MonoBehaviour {
+	
+	static string CommPort = "COM5";
+	static int BaudRate = 9600;
 
-	public string CommPort = "COM5";
-	public int BaudRate = 9600;
-	public byte[] buf = new byte[3 * 2];
+	public static Vector3 accelerometer
+	{
+		get { return a == null ? Vector3.zero : a; }
+	}
+	public static Vector3 gyroscope
+	{
+		get { return g == null ? Vector3.zero : g; }
+	}
 
-	SerialPort Serial;
+	static Vector3 a, g;
+	static SerialPort Serial;
+	static string buffer = "";
 
 	// Use this for initialization
 	void Start () {
@@ -19,16 +30,29 @@ public class Arduino : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () { 
-		if (!Input.GetKey ("space")) return;
-		Debug.Log (Serial.ReadLine ());
+	void Update () {
+		if (Serial == null || !Serial.IsOpen)
+			return;
+		new Thread (Read).Start ();
+		Parse ();
 	}
 
-	int[] BytesToShorts(byte[] b) {
-		int[] s = new int[b.Length / 2];
-		for (int i = 0; i < b.Length; i += 2) {
-			s[i / 2] = ((int) b[i] << 8) | b[i + 1];
-		}
-		return s;
+	void Read() {
+		buffer = Serial.ReadLine ();
 	}
+
+	void Parse() {
+		try {
+			string currentBuffer = buffer;
+			string[] sp = currentBuffer.Split (' ');
+			float[] f = new float[sp.Length];
+			for (int i = 0; i < sp.Length; ++i) {
+				f[i] = float.Parse (sp[i]);
+			}
+			a = new Vector3(f[0], f[1], f[2]);
+			g = new Vector3(f[3], f[4], f[5]);
+		}
+		catch {}
+	}
+
 }
